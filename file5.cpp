05755fd8682c51302f48b5e1542d39cc4b2a9e21
Miranti20 <rantirarrr4505@gmail.com>
2025-05-13 11:11:33 +0700

    membuat int main

--- file5.cpp
+++ file5.cpp
@@ -27,5 +27,18 @@ mahasiswa :: mahasiswa(string iNama){
 }
 
 void mahasiswa::cetak(){
+    cout << endl << "NIM = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("andi");
+    mahasiswa mhs3(010203, "Santi"); 
     
+    cout << "Mahasiswa 1: "; mhs1.cetak();
+    cout << "Mahasiswa 2: "; mhs2.cetak();
+    cout << "Mahasiswa 3: "; mhs3.cetak();
+
+    return 0;
 }
