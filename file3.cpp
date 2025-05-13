b30ea612717b9a87000330ac00869c50e2af1ca2
Miranti20 <rantirarrr4505@gmail.com>
2025-05-13 10:41:53 +0700

    Memanggil Constructor dengan parameter

---
+++ file3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class mahasiswa {
+    public:
+    mahasiswa(int nim, string nama);  // Constructor dengan parameter
+};
+// Defenisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Paramater Terpanggil"<<endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nim : " << nama << endl;
+}
+int main (){
+    mahasiswa mhs(12345, "Abra") // Memanggil constructor dengan parameter
+    return0;
+}
